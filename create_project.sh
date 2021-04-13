d91fb76d3d25afbd63dc62f0153d83ac41e732f8
Rodrigo Cabaço <42916568+RodrigoCabaco@users.noreply.github.com>
2021-04-13 18:49:14 +0100

    Update project creation

---
+++ create_project.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+mkdir ../rC_Framework_Project
+cp -r ./default_project/* ../rC_Framework_Project/
+#rm -rf ../rC_Framework_Project/create_project.sh
+echo Project Created at ../rC_Framework_Project
